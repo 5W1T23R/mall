9fbe94c7ac0b5f1a8e2570e0c3413b594b9aa124
smart-202-14 <smart-202-14@smart-202-14-PC>
2018-10-19 15:08:01 +0900

    [김준성] 2018-10-19 15:06
    1. AddMember Controller생성
    2. MemberDao생성

--- mall/src/main/java/cafe/jjdev/mall/controller/IndexController.java
+++ mall/src/main/java/cafe/jjdev/mall/controller/IndexController.java
@@ -1,11 +1,15 @@
 package cafe.jjdev.mall.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import cafe.jjdev.mall.service.Member;
 
 @WebServlet("/index")
 public class IndexController extends HttpServlet {
@@ -18,7 +22,17 @@ public class IndexController extends HttpServlet {
 		String model = "jjdev";
 		// view rend...(template)
 
-		request.setAttribute("model", model);
+		request.setAttribute("model", model); //${model}
+		Member member = new Member();
+		member.setNo(1); member.setId("guest"); member.setLevel(0);
+		request.setAttribute("member", member); //${member.id}
+		
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("승정"); list.add("승우"); list.add("찬우");
+		request.setAttribute("list", list); //${list}
+		
+		
+		
 
 		// 4. Forward(request, response) to WEB-INF/jsp/index.jsp
 		request.getRequestDispatcher("WEB-INF/jsp/index.jsp").forward(request, response);
--- mall/src/main/webapp/WEB-INF/jsp/index.jsp
+++ mall/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
-<%@ page isELIgnored="false" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page isELIgnored="false"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,7 +9,20 @@
 <title>Insert title here</title>
 </head>
 <body>
-	Hello <%=request.getAttribute("model")%><br>
-	Hello ${model}
+	<%-- 	Hello
+	<%=request.getAttribute("model")%> --%>
+	<br> Hello ${model}
+	<br>
+	<!-- if(){} -->
+	<c:if test="${member.level == 0 }">
+		일반고객
+	</c:if>
+	<c:if test="${member.level == 1 }">
+		관리자
+	</c:if>
+	<!-- for(String s : err) -->
+	<c:forEach var="str" items="${list}">
+		<div>${str}</div>
+	</c:forEach>
 </body>
 </html>
