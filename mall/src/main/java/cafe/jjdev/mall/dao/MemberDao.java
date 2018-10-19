319fd4206d4da7855ff608716545a15f43396e66
smart-202-14 <smart-202-14@smart-202-14-PC>
2018-10-19 17:07:31 +0900

    [김준성] 2018-10-19 17:07

---
+++ mall/src/main/java/cafe/jjdev/mall/dao/MemberDao.java
@@ -0,0 +1,14 @@
+package cafe.jjdev.mall.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.sql.DataSource;
+
+public class MemberDao {
+DataSource ds;
+Connection conn;
+PreparedStatement pstmt;
+ResultSet rs;
+}
